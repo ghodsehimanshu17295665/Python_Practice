53c15aaf74e39c25a7193086098386f9951fd2db
Himanshu <himanshughodse@gmail.com>
2024-07-18 17:50:02 +0530

    Sort & Sorted

---
+++ 05_sort_sorted.py
@@ -0,0 +1,19 @@
+
+list1 = [1, 5, 4, 2, 3]
+
+# Print the original list
+print("Original list after sorting:")
+print(list1)
+
+# Print the sorted list
+print("Sorted list:")
+print(sorted(list1))
+
+
+# List of Integers
+numbers = [1, 3, 4, 2]
+print("\nOriginal List:", numbers)
+numbers.sort()
+print("Sorted List (Acending Order):", numbers)
+numbers.sort(reverse=True)
+print("Sorted List (Descending Order):", numbers)
